a173771a9ccd137cb893fa3516361bce5215e56f
zhang1998 <2764207312@qq.com>
2018-11-01 21:40:10 +0800

    不清楚的修改

--- init.el
+++ init.el
@@ -13,3 +13,18 @@
 (setq w3m-use-cookies t)
 ;设定w3m运行的参数，分别为使用cookie和使用框架 
 (setq w3m-command-arguments '("-cookie" "-F"))
+
+;;orgmode的部分设置
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
