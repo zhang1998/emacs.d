57bc044fd65626ec06f8197d7310ba41473604ed
zhang1998 <2764207312@qq.com>
2018-11-19 20:39:26 +0800

    common

--- init.el
+++ init.el
@@ -1,9 +1,18 @@
 
 
 ;;修改：eww搜索引擎
-(setq eww-search-prefix "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq eww-search-prefix "https://www.baidu.com/")
 
 
+;;设置w3
+ (add-hook 'w3-parse-hooks 'w3-tidy-page)
 
 
 
@@ -12,7 +21,7 @@
 ;;支持w3m
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m-master") 
 (require 'w3m)
-(setq w3m-home-page "http://www.baidu.com") ;设置主页 
+(setq w3m-home-page "http://www.baidu.com/") ;设置主页 
 ;(require 'mime-w3m) 
 ;设置显示图片
 (setq w3m-default-display-inline-images t)
@@ -24,7 +33,8 @@
 (setq w3m-use-cookies t)
 ;设定w3m运行的参数，分别为使用cookie和使用框架 
 (setq w3m-command-arguments '("-cookie" "-F"))
-
+;按设置搜索引擎
+(setq w3m-search-default-engine "yahoo")
 
 
 
