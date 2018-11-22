82e8a6f5e0d364f6eaf03545b8c61e229bf34500
zhang1998 <2764207312@qq.com>
2018-11-22 16:20:07 +0800

    common

--- init.el
+++ init.el
@@ -25,8 +25,22 @@
     (reapply-themes) ) ) ;; end of (defun ... 
 (run-with-timer 0 3600 'synchronize-theme)
 
-
-
+;;;orgmode 自动折行
+;orgmode的部分设置
+;;自动折行
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
 
 
 ;;; This file bootstraps the configuration, which is divided into
