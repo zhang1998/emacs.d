53882ecb980055fe0e58d1ab5deb62a86fde483e
zhang1998 <2764207312@qq.com>
2018-12-05 18:18:44 +0800

    将自己的配置修改为local

--- init.el
+++ init.el
@@ -4,10 +4,6 @@
 ;;;为了成功安装进行了尝试
 ;;; Standard package repositories
 
-;;;修改emacsw3m主页问题
-(setq w3m-home-page "https://cn.bing.com/")
-(setq w3m-search-default-engine "bing")
-
 
 
 
@@ -26,23 +22,6 @@
 ;;(run-with-timer 0 3600 'synchronize-theme)
 
 
-
-;;;orgmode 自动折行
-;orgmode的部分设置
-;;自动折行
-(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
 
 
 ;;; This file bootstraps the configuration, which is divided into
@@ -220,9 +199,6 @@
 
 (provide 'init)
 
-
-;;add indent
-(setq org-startup-indented t)
 ;; Local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
---
+++ lisp/init-local.el
@@ -0,0 +1,31 @@
+;;;... your code here ...
+
+;;;修改emacsw3m主页问题
+(setq w3m-home-page "https://cn.bing.com/")
+(setq w3m-search-default-engine "bing")
+
+    ;;(reapply-themes) ) ) ;; end of (defun ...
+;;(run-with-timer 0 3600 'synchronize-theme)
+
+
+
+;;;orgmode 自动折行
+;orgmode的部分设置
+;;自动折行
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+;;add indent
+(setq org-startup-indented t)
+
+(provide 'init-local)
