a13b3e57ecbe6e92667cc5daaa700e5cd5f8c3f7
zhang1998 <2764207312@qq.com>
2018-12-11 12:41:57 +0800

    添加sdcv查询 c-c s

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,7 +7,34 @@
     ;;(reapply-themes) ) ) ;; end of (defun ...
 ;;(run-with-timer 0 3600 'synchronize-theme)
 
+;;添加英语单词服务
 
+(global-set-key (kbd "C-c s") 'kid-sdcv-to-buffer) 
+(defun kid-sdcv-to-buffer () 
+  (interactive) 
+  (let ((word (if mark-active 
+		(buffer-substring-no-properties (region-beginning) (region-end)) 
+		(current-word nil t)))) 
+    (setq word (read-string (format "Search the dictionary for (default %s): " word) 
+			    nil nil word)) 
+    (set-buffer (get-buffer-create "*sdcv*")) 
+    (buffer-disable-undo) 
+    (erase-buffer) 
+    (let ((process (start-process-shell-command "sdcv" "*sdcv*" "sdcv" "-n" word))) 
+      (set-process-sentinel 
+	process 
+	(lambda (process signal) 
+	  (when (memq (process-status process) '(exit signal)) 
+	    (unless (string= (buffer-name) "*sdcv*") 
+	      (setq kid-sdcv-window-configuration (current-window-configuration)) 
+	      (switch-to-buffer-other-window "*sdcv*") 
+	      (local-set-key (kbd "d") 'kid-sdcv-to-buffer) 
+	      (local-set-key (kbd "q") (lambda () 
+					 (interactive) 
+					 (bury-buffer) 
+					 (unless (null (cdr (window-list))) ; only one window 
+					   (delete-window))))) 
+	    (goto-char (point-min))))))))
 
 ;;;orgmode 自动折行
 ;orgmode的部分设置
